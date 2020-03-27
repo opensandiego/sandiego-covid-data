1a1d988124c7ac0c0dfa9129c7c52424fb8ec362
Nikolaj Baer <nikolaj.baer@gmail.com>
2020-03-26 20:33:57 -0700

    initial commit

---
+++ scrape_sdcounty_status.py
@@ -0,0 +1,36 @@
+import requests,html5lib,json
+
+
+def get_county_html():
+    URL = "https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html"
+    resp = requests.get("https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html")
+    html = html5lib.parse(resp.content,namespaceHTMLElements=False)
+    return html
+
+def parse_out_table(doc):
+    data = []
+    k = None
+    for tr in doc.findall('.//table')[0].findall('.//tr'):
+        r = [ e.text for e in tr.findall('.//td/b')]
+        if not r or r[0] == None:
+            r =  [k] + [ e.text for e in tr.findall('.//td')]
+            data.append(r)
+        else:
+            k = r[0]
+            if len(r) > 1:
+                data.append(r)
+
+    result = {}
+    for r in data:
+        if len(r) == 2 and r[0]:
+            result[r[0]] = r[1] 
+        elif len(r) == 3:
+            result.setdefault(r[0],{})
+            result[r[0]][r[1]] = int(r[2])
+    return result
+
+if __name__=="__main__":
+    htmldoc = get_county_html() 
+    data = parse_out_table(htmldoc)
+    print( json.dumps(data,indent=1) )
+
