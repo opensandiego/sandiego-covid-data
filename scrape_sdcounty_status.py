91bd76ed6b306a96ca933dd1c76944f5ffa8b73e
Nikolaj Baer <nikolaj.baer@gmail.com>
2020-03-28 09:31:35 -0700

    updates to lambda task to better track over time and store a manifest

--- scrape_sdcounty_status.py
+++ scrape_sdcounty_status.py
@@ -1,49 +1,97 @@
 import requests,html5lib,json
-import boto3,datetime
+import boto3,datetime,sys,re
 
+URL = "https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html"
 
-def get_county_html():
-    URL = "https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html"
-    resp = requests.get("https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html")
+def get_county_html(url=None):
+    if not url:
+        url = URL
+    resp = requests.get(url)
     html = html5lib.parse(resp.content,namespaceHTMLElements=False)
     return html
 
 def parse_out_table(doc):
     data = []
     k = None
-    for tr in doc.findall('.//table')[0].findall('.//tr'):
-        r = [ e.text for e in tr.findall('.//td/b')]
-        if not r or r[0] == None:
-            r =  [k] + [ e.text for e in tr.findall('.//td')]
-            data.append(r)
-        else:
-            k = r[0]
-            if len(r) > 1:
-                data.append(r)
+    for tr in doc.findall('.//div/table')[0].findall('.//tr'):
+        r = [ (e.findall('.//b') and e.findall('.//b')[0].text or e.text)for e in tr.findall('.//td')]
+        data.append(r)
 
     result = {}
     for r in data:
-        if len(r) == 2 and r[0]:
-            result[r[0]] = r[1] 
-        elif len(r) == 3:
-            result.setdefault(r[0],{})
-            result[r[0]][r[1]] = int(r[2])
-    result["updated"] = datetime.datetime.now().isoformat()
+        if len(r) <= 1: continue
+        k = r[0]
+        v = r[1].strip()
+        if not v: continue
+        if len(r) == 4:
+            v = r[3]
+        try:
+            v = int(v)
+        except ValueError:
+            pass
+        #print("%s = %s" % (k,v))
+        result[k] = v
+
+    result["updated"] = (datetime.datetime.utcnow() - datetime.timedelta(hours=7)).strftime("%Y-%m-%d")
     return result
 
 if __name__=="__main__":
-    htmldoc = get_county_html() 
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    else: url = None
+    
+    htmldoc = get_county_html(url) 
     data = parse_out_table(htmldoc)
     print( json.dumps(data,indent=1) )
 
 def lambda_handler(event, context):
     s3_client = boto3.client('s3')
 
-    path = 'data/coronavirus-latest.json'
+    path = 'data/corona-sd/coronavirus-latest.json'
+    dated_path = 'data/corona-sd/%s-coronavirus.json'
     bucket = 'opensandiego-data'
 
     # Get Data
     htmldoc = get_county_html() 
     data = parse_out_table(htmldoc)
-    s3_client.put_object( Body = json.dumps(data,indent=1), Bucket=bucket, Key=path )
+
+    s3_client.put_object( 
+        Body = json.dumps(data,indent=1), 
+        Bucket=bucket, 
+        Key=path, 
+        ContentType="application/json",
+        ACL='public-read',
+    )
+    s3_client.put_object( 
+        Body = json.dumps(data,indent=1), 
+        Bucket=bucket, 
+        ContentType="application/json",
+        Key=dated_path % datetime.datetime.now().strftime("%Y-%m-%d"), 
+        ACL='public-read',
+    )
+
+    # Generate index
+    # TODO
+    result = s3_client.list_objects_v2(
+        Bucket = bucket,
+        Prefix = 'data/corona-sd/',
+    )
+    # Generate listing with date stamp  
+    listing = []
+    regex = re.compile(r'data\/corona-sd\/(\d+)-(\d+)-(\d+)-corona-sd.json')
+    for d in result['Contents']:
+        m = regex.match(d['Key'])
+        if m:
+            listing.append( { 
+                "date":zip(('year','month','day'),m.groups()), 
+                "path": d['Key']
+            })
+    s3_client.put_object( 
+        Body = json.dumps(listing,indent=1), 
+        Bucket=bucket, 
+        Key='data/corona-sd/manifest.json', 
+        ContentType="application/json",
+        ACL='public-read',
+    )
+
 
